f252bb83e43d53b7cf42cc290e19dbdc5a3f5cc8
Marie-AnneS <marie-anne.seim@gmail.com>
2019-06-12 01:38:27 +0000

    exercise 6 with the relation belong

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,7 +1,3 @@
 class Employee < ActiveRecord::Base
-  # has_many :first_name
-  # has_many :last_name
-  # has_many :hourly_rate
-  # has_many :timestamps
-  # has_many :store, through: :employees 
+  belongs_to :store 
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,8 +1,3 @@
 class Store < ActiveRecord::Base
-  # has_many :name
-  # has_many :annual_revenue
-  # has_many :mens_apparel
-  # has_many :womens_apparel
-  # has_many :timestamps
-  
+  has_many :employees
 end
